20e545208236a30b742936c341ddcddee7e06d26
AMF064 <114866865+AMF064@users.noreply.github.com>
2024-04-11 22:29:26 +0200

    Minor changes on adc.c:
    1. Disable interrupts
    2. SQRx fields are 5 bits long

--- include/adc.c
+++ include/adc.c
@@ -2,10 +2,11 @@
 
 void adc_init(uint8_t scan_mode_on, Resolution res, uint8_t channel)
 {
-    ADC1->CR2 &= ~0x1;
+    ADC1->CR2 &= ~0x1;      // ADON
+    ADC1->CR1 &= 0;         // Disable interrupts
     if (scan_mode_on) ADC1->CR1 |= (1 << SCAN);
     set_bits_in_32_register(&ADC1->CR1, res, 2, RES);
     ADC1->SMPR1 = ADC1->SMPR2 = ADC1->SMPR3 = 0;
     ADC1->SQR1 = 0;
-    set_bits_in_32_register(&ADC1->SQR5, channel, 4, 0);
+    set_bits_in_32_register(&ADC1->SQR5, channel, 5, 0);
 }
